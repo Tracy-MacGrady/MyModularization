0fa91412e87696648da8044a745124d122cacd2a
Tracy-MacGrady <1194787489@qq.com>
2018-06-29 11:41:36 +0800

    update project

--- downloadlibrary/src/main/java/com/qingguo/downloadlib/DownloadManager.java
+++ downloadlibrary/src/main/java/com/qingguo/downloadlib/DownloadManager.java
@@ -2,6 +2,7 @@ package com.qingguo.downloadlib;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
